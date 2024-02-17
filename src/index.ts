0a09b952e13146ceb801734b918d04fc06870684
Roshan <lamichhaner40@gmail.com>
2024-02-17 14:27:37 -0500

    Added Database

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 // src/index.ts
 import express from 'express';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
 
 const app = express();
 const port = 3000;
 
-// MongoDB connection string
-const mongoUrl = 'your_mongodb_connection_string_here';
+// MongoDB connection DBURL
+const DB_URL = process.env.DBURL || 'mongodb+srv://gymbell:ItsSafeAndLit24@cluster0.yg4q8go.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(DB_URL);
 
-// Connect to MongoDB
-/*const client = new MongoClient(mongoUrl);
 client.connect().then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => console.error(err));*/
+    console.log('Connected to MongoDB');
+    }
+).catch((error) => {    
+    console.log('Error connecting to MongoDB', error);
+    }   
+);
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript with Express and MongoDB!');
